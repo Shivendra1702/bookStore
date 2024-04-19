b3a0e800c253a2c43fee7923d23b85935007df69
Shivendra Pratap Jadia <shivendrapratapjadia@gmail.com>
2024-04-19 12:46:13 +0530

    add user login route

--- server/src/user/userController.ts
+++ server/src/user/userController.ts
@@ -10,37 +10,86 @@ export const createUser = async (
   res: Response,
   next: NextFunction
 ) => {
-  //validation
-  const { name, email, password } = req.body;
+  try {
+    //validation
+    const { name, email, password } = req.body;
 
-  if (!name || !email || !password) {
-    const error = createHttpError(400, "Please Enter all fields !");
-    return next(error);
+    if (!name || !email || !password) {
+      const error = createHttpError(400, "Please Enter all fields !");
+      return next(error);
+    }
+
+    //database call
+    const user = await User.findOne({ email });
+
+    if (user) {
+      const error = createHttpError(400, "User Already Exists !");
+      return next(error);
+    }
+
+    //password hashing
+    const hashedPassword = await bcrypt.hash(password, 12);
+
+    //process
+    const newUser = await User.create({
+      name,
+      email,
+      password: hashedPassword,
+    });
+
+    //token generation
+    const token = sign({ id: newUser._id }, config.jwtSecret as string, {
+      expiresIn: "7d",
+    });
+
+    //response
+    return res.status(200).json({
+      id: newUser._id,
+      accessToken: token,
+      ok: true,
+    });
+  } catch (error) {
+    return next(createHttpError(500, "Internal Server Error !!"));
   }
+};
 
-  //database call
-  const user = await User.findOne({ email });
+export const loginUser = async (
+  req: Request,
+  res: Response,
+  next: NextFunction
+) => {
+  try {
+    //validation
+    const { email, password } = req.body;
+    if (!email || !password) {
+      return next(createHttpError(400, "Please Enter all fields !"));
+    }
 
-  if (user) {
-    const error = createHttpError(400, "User Already Exists !");
-    return next(error);
+    //database
+    const user = await User.findOne({ email });
+    if (!user) {
+      return next(createHttpError(400, "Invalid Credentials !"));
+    }
+
+    //password verification
+    const isMatch = await bcrypt.compare(password, user.password);
+
+    if (!isMatch) {
+      return next(createHttpError(400, "Invalid Credentials !"));
+    }
+
+    //token generation
+    const token = sign({ id: user._id }, config.jwtSecret as string, {
+      expiresIn: "7d",
+    });
+
+    //response
+    return res.status(201).json({
+      id: user._id,
+      accessToken: token,
+      ok: true,
+    });
+  } catch (error) {
+    return next(createHttpError(500, "Internal Server Error !!"));
   }
-
-  //password hashing
-  const hashedPassword = await bcrypt.hash(password, 12);
-
-  //process
-  const newUser = await User.create({ name, email, password: hashedPassword });
-
-  //token generation
-  const token = sign({ id: newUser._id }, config.jwtSecret as string, {
-    expiresIn: "7d",
-  });
-
-  //response
-  return res.json({
-    id: newUser._id,
-    accessToken: token,
-    message: "User Created Successfully !",
-  });
 };
